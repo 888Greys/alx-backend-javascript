35e738a693d5fb2d027ef379b8ddc5092c83c608
888Greys <toxicgreys001@gmail.com>
2024-06-21 02:16:49 +0300

    update

---
+++ 0x06-unittests_in_js/2-calcul_chai.js
@@ -0,0 +1,26 @@
+/**
+ * Performs a mathematical operation on two numbers.
+ * @param {string} type - The type of operation to perform. Valid values are 'SUM', 'SUBTRACT', and 'DIVIDE'.
+ * @param {number} a - The first number.
+ * @param {number} b - The second number.
+ * @returns {number|string} - The result of the operation. If the operation is 'DIVIDE' and the second number is 0, returns 'Error'.
+ */
+function calculateNumber(type, a, b) {
+    const rounded_a = Math.round(a);
+    const rounded_b = Math.round(b);
+    
+    if (type === 'SUM') {
+      return rounded_a + rounded_b;
+    }
+    if (type === 'SUBTRACT') {
+      return rounded_a - rounded_b;
+    }
+    if (type === 'DIVIDE') {
+      if (rounded_b === 0) {
+        return 'Error';
+      }
+      return rounded_a / rounded_b;
+    }
+  }
+  
+  module.exports = calculateNumber;
