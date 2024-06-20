4d838a27b822595d28d5b812ed364fcbbfeeed84
888Greys <toxicgreys001@gmail.com>
2024-06-21 02:11:23 +0300

    update

--- 0x06-unittests_in_js/1-calcul.js
+++ 0x06-unittests_in_js/1-calcul.js
@@ -1,10 +1,3 @@
-/**
- * Performs a mathematical operation on two numbers.
- * @param {string} type - The type of operation to perform. Possible values are 'SUM', 'SUBTRACT', and 'DIVIDE'.
- * @param {number} a - The first number.
- * @param {number} b - The second number.
- * @returns {number|string} - The result of the operation. If the operation is 'DIVIDE' and the second number is 0, returns 'Error'.
- */
 function calculateNumber(type, a, b) {
     const rounded_a = Math.round(a);
     const rounded_b = Math.round(b);
