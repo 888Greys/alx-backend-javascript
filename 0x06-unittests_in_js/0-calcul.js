eb5ec72207bb5c1299a2b71edb40a2c8419c703f
888Greys <toxicgreys001@gmail.com>
2024-06-21 02:06:49 +0300

    update

--- 0x06-unittests_in_js/0-calcul.js
+++ 0x06-unittests_in_js/0-calcul.js
@@ -1,12 +1,5 @@
-/**
- * Calculates the sum of two numbers.
- * @param {number} a - The first number.
- * @param {number} b - The second number.
- * @returns {number} The sum of the two numbers.
- */
 function calculateNumber(a, b){
     return Math.round(a) + Math.round(b);
 }
 
 module.exports = calculateNumber;
-
